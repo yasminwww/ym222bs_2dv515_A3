573a74ee30153d3255a092775804c23e46e334de
Yasmin <ym222bs@student.lnu.se>
2019-12-03 19:49:48 +0100

    finished with filereading and jsons

---
+++ txtFileReader.js
@@ -0,0 +1,63 @@
+'use strict'
+const path = require('path')
+const fs = require('fs')
+const Stream = require('stream')
+const readLine = require('readline')
+const es = require('event-stream')
+const dirPath = path.join(__dirname, 'wikipedia/Words')
+
+/**
+ * @param {filePath} folders iterate every file
+ */
+const iterateFolders = () => {
+    try {
+        fs.readdir(dirPath, (err, folders) => {
+            if (err) throw err
+            folders.forEach((filePath) => {
+                
+                fs.readdir(`${dirPath}/${filePath}`, (err, files) => {
+                    if (err) throw err
+                    files.forEach(file => {
+                        fileReader(path.join(`${dirPath}/${filePath}/${file}`)).then((wordOccurences) => {
+                            fs.writeFile('wordCount.json', JSON.stringify(wordOccurences, null, 4), 'utf8', err => {
+                                if (err) throw err
+                                console.log('success')
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    } catch (err) {
+        throw err
+    }
+}
+const fileReader = (file) => {
+    return new Promise((resolve, reject) => {  
+    let data = ''
+    let result = {}
+    let readStream = fs.createReadStream(file, 'utf8')
+
+    readStream.on('data', chunk => {
+        data += chunk
+        console.log(data)
+    })
+        .on('end', () => {
+            result = data.replace(/[.]/g, '')
+                .split(/\s/)
+                .reduce((map, word) =>
+                    Object.assign(map, {
+                        [word]: (map[word]) ? map[word] + 1 : 1
+                    }),
+                    {}
+                )
+            resolve(result)
+        })
+    })
+}
+
+iterateFolders(dirPath)
+
+// const file = fs.createReadStream((`C:/Users/Mosa/School/arkurs3/2dv515/assignment3/kr222if-ym222bs-2dv515-assignment3/wikipedia/Words/Games/7400_series`, 'utf8')).pipe(es.mapSync((line) => {
+//     console.log(line)
+// }))
---
+++ txtToJSON.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const path = require('path')
+
+
+const stringifyArray = (result) => {
+    fs.writeFile('./wordCounts.json', JSON.stringify(result, null, 2), err => {
+        if (err) throw err
+    
+        console.log('Successfully wrote file')
+
+    })
+}
+
+
+const iterateDirectory = () => {
+    let arr = []
+    const directoryPath = path.join(__dirname, 'wikipedia/Words')
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) console.log('Unable to scan directory: ' + err)
+
+        files.forEach(async function (file) {
+            let filePathArray = file.split(' ')
+            filePathArray.forEach(filestring => {
+                let obj = {}
+                obj[filestring] = fileReader(path.join(`${directoryPath}/${filestring}`))
+                arr.push(obj)
+            })
+        })
+        stringifyArray(arr)
+    })
+}
+
+
+const fileReader = (file) => {
+    try {
+        const readStream = fs.readFileSync(file, 'utf8')
+        const words = readStream.replace(/[.]/g, '').split(/\s/)
+
+        const frequencyMap = {}
+        words.forEach(w  => {
+            if (!frequencyMap[w]) {
+                frequencyMap[w] = 0
+            }
+            frequencyMap[w] += 1
+        })
+        
+        return frequencyMap
+
+    // Wiie
+    // const map = words.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map())
+
+    // let map = new Map([...new Set(words)].map(
+    //     x => [x, words.filter(y => y === x).length]
+    // ))
+    // console.log(map)
+
+    } catch (error) {
+        console.log(error)
+    }
+  
+}
+
+
+// console.log(iterateDirectory())
+
+iterateDirectory()
